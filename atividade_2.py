175d46a75c2a4581e401e298296309aa7e0aa8ef
MatheusFVieira <matheus.vf06@gmail.com>
2025-03-19 22:28:29 -0300

    Atividade_2

---
+++ atividade_2.py
@@ -0,0 +1,27 @@
+# Exercício 2
+
+#Letra A
+
+imposto_1 = 2500 * 0.10
+sal_recebido_1 = 2500 * 0.90
+
+print("Imposto recebido é: R$", imposto_1)
+print("Salario recebido é: R$", sal_recebido_1)
+
+
+#Letra B
+
+imposto_2 = 3500 * 0.15
+sal_recebido_2 = 3500 * 0.85
+
+print("Imposto recebido é: R$", imposto_2)
+print("Salario recebido é: R$", sal_recebido_2)
+
+
+#Letra C
+
+imposto_3 = 5000 * 0.25
+sal_recebido_3 = 5000 * 0.75
+
+print("Imposto recebido é: R$", imposto_3)
+print("Salario recebido é: R$", sal_recebido_3)
