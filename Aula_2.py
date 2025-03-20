a23f8870d6432a20cf0e8fe935f27a91f8a5e382
MatheusFVieira <matheus.vf06@gmail.com>
2025-03-19 22:22:43 -0300

    Atividade 1

--- Aula_2.py
+++ Aula_2.py
@@ -1,40 +1,20 @@
-# Exercício 1
+# Exercício 1:
 
 a = float(input("Digite o valor de A:\n"))
 b = float(input("Digite o valor de B:\n"))
 c = float(input("Digite o valor de C:\n"))
 
-#Letra A
+# Letra A
 
 result_1 = a + b + c
 print(result_1)
 
-
-#Letra B
+# Letra B
 
 result_2 = a - b + c
 print(result_2)
 
-
-#Letra C
+# Letra C
 
 result_3 = a**2 + 2*b - 5.1*c
 print(result_3)
-
-
-#Letra D
-
-result_4 = a + b == c
-print(result_4)
-
-
-#Letra E
-
-result_5 = c + a == b
-print(result_5)
-
-
-#Letra F
-
-result_6 = a + c == b - c
-print(result_6)
