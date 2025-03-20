7afdbc3434c8155b960faf7b22ddf3849ff2e91d
MatheusFVieira <matheus.vf06@gmail.com>
2025-03-19 22:38:25 -0300

    Inicio

--- Aula_2.py
+++ Aula_2.py
@@ -1,20 +1,93 @@
+# Ex.1
+
+# Estrutura de entrada
+
+variavel_A = 20
+variavel_B = 10
+
+# Estrutura de saída
+
+print(f'Valor da variável A é {variavel_A}')
+print(f'Valor da variável B é {variavel_B}')
+
+# Estrutura de condição
+
+if variavel_A > variavel_B:
+    print("A é maior que B")
+elif variavel_A == variavel_B:
+    print("A e B são iguais!")
+else:
+    print("B é maior que A")
+    
+    
+
+# Ex.2 - Estrutura com input para o usuário
+
+var_A = float(input("Insira o valor de A:\n"))
+var_B = float(input("Insira o valor de B:\n"))
+
+# Estrutura de condição
+
+if var_A > var_B:
+    print("A é maior que B")
+elif var_A == var_B:
+    print("A e B são iguais!")
+else:
+    print("B é maior que A")
+    
+    
+    
+# Ex.3 - Variáveis mão numéricas
+
+usuario = str(input("Digite um caractere ou palavra:\n"))
+senha = str(input("Digite sua senha:\n"))
+
+print(usuario)
+print(senha)
+
+
+# Ex.4 - Cálculos com variáveis constantes e variáveis com input do usuário
+
+x = 10.5
+resultado = (2*x) + 10
+print(resultado)
+
+
+# Ex.5
+
+x1 = float(input("Digite um valor:\n"))
+resultado_1 = (2*x1) + 10
+print(resultado_1)
+
+
+
+
+# Ex.6 - Crie um algoritmo para calcular o resultado das sequintes equações:
+    
+# 1) 10x^2 + 2x - 2
+# 2) 5x^2 + 3y + 5
+# 3) 10x^2 + 4x^2 - 2z^2
+
 # Exercício 1:
+    
+x = float(input("Digite o valor de X:\n"))
+y = float(input("Digite o valor de Y:\n"))
+z = float(input("Digite o valor de Z:\n"))
 
-a = float(input("Digite o valor de A:\n"))
-b = float(input("Digite o valor de B:\n"))
-c = float(input("Digite o valor de C:\n"))
+resultado = 10*(x**2) + (2*x) -2
 
-# Letra A
+print(resultado)
+          
 
-result_1 = a + b + c
-print(result_1)
+# Exercício 2:
 
-# Letra B
+resultado_2 = 5*(x**2) + (3*y) + 5
 
-result_2 = a - b + c
-print(result_2)
+print(resultado_2)
 
-# Letra C
 
-result_3 = a**2 + 2*b - 5.1*c
-print(result_3)
+# Exercício 3:
+
+resultado_3 = 10*(x**2) + 4*(y**2) - 2*(z**2)
+
+print(resultado_3)
