2e7e451c6ce5dd4a4968e15e7dd597ee1c3cd240
MatheusFVieira <matheus.vf06@gmail.com>
2025-03-19 22:36:22 -0300

    Atividade_3

---
+++ atividade_3.py
@@ -0,0 +1,13 @@
+# Exercício 3
+
+email_A = str(input("Digite o email do primeiro usuario:\n"))
+senha_A = str(input("Digite a senha do primeiro usuario:\n"))
+palavra_A = str(input("Digite a palavra chave do primeiro usuario:\n"))
+
+email_B = str(input("Digite o email do segundo usuario:\n"))
+senha_B = str(input("Digite a senha do segundo usuario:\n"))
+palavra_B = str(input("Digite a palavra chave do segundo usuario:\n"))
+
+print(f'\nEmail primeiro usuario: {email_A}\n Senha primeiro usuario: {senha_A}\n Palavra chave primeiro usuario: {palavra_A}')
+
+print(f'\nEmail segundo usuario: {email_B}\n Senha segundo usuario: {senha_B}\n Palavra chave segundo usuario: {palavra_B}')
