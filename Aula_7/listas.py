bcf4cc2c25aeed241c5c3e172f86abe7ce9a7653
MatheusFVieira <matheus.vf06@gmail.com>
2025-04-23 22:26:40 -0300

    Aula_7

---
+++ Aula_7/listas.py
@@ -0,0 +1,27 @@
+# Lista
+
+lista_nome = ['ana', 'matheus', 'pedro', 'melissa']
+lista_cep = ['111', '222', '333', '444']
+
+print(lista_nome)
+print(lista_cep)
+
+
+# Dicionario
+
+dicionario = {
+    
+    'nome': ['ana', 'matheus', 'pedro', 'melissa'],
+    'cep': ['111', '222', '333', '444']
+    
+}
+
+print(dicionario)
+
+
+# Pandas
+
+import pandas as pd
+
+dataframe = pd.DataFrame(dicionario)
+dataframe
